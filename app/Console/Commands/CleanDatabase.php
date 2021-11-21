5e244658ef4a22fb5c17a27ac9dc3df47ff8d233
yassine loukhnati <yassine.jordan@gmail.com>
2021-11-21 20:03:43 +0100

    create command clean-database and creating scope and schedule task

---
+++ app/Console/Commands/CleanDatabase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Models\Message;
+
+class CleanDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laracarte:clean-database';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean Database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->info('Cleaning database...');
+
+        Message::TwoMonthsOld()->delete();
+
+        $this->info('database cleaned');
+    }
+}
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Carbon\Carbon;
 
 class Message extends Model
 {
@@ -11,4 +13,8 @@ class Message extends Model
 
     protected $fillable = ['name','email','message'];
 
+    public function scopeTwoMonthsOld(Builder $query)
+    {
+        $query->where('created_at', '<=', Carbon::parse('2 months ago'));
+    }
 }
